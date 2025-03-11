cf251f7a011b98276f59cb6b0d3ee163fb61b242
vbenv <tjdcksl20@gmail.com>
2025-03-11 19:58:18 +0900

    week02 kiosk

---
+++ week02.py
@@ -0,0 +1,10 @@
+# 1) Ice Americano : 2000 2) Cafe Latte : 3000
+menu = input("1) Ice Americano  2) Cafe Latte   3) Exit : ")
+
+if menu == "1":
+    print(f'Ice Americano ordered. Price : 2000 won')
+elif menu == "2":
+    print('Cafe Latte ordered. Price : 3000 won')
+elif menu == "3":
+    print("Finish order~")
+    
