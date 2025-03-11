2a2dc4dcf7b609cb5e632326219cc584c7864d58
vbenv <tjdcksl20@gmail.com>
2025-03-11 23:29:09 +0900

    _

--- week02.py
+++ week02.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # 1) Ice Americano : 2000 2) Cafe Latte : 3000
 drinks = ["Ice Americano", "Cafe Latte"]
 prices = [2000, 3000]
@@ -12,6 +13,8 @@ while True:
         print(f'{drinks[1]} ordered. Price : {prices[1]} won\n')
         total_price = total_price + prices[1]
 =======
+=======
+>>>>>>> dev01
 # 1) Ice Americano : 2000  2) Cafe Latte : 3000
 drinks = ["Ice Americano", "Cafe Latte"]
 prices = [2000, 3000]
@@ -27,13 +30,22 @@ while True:
         print(f"{drinks[1]} ordered. Price : {prices[1]}won")
         total_price = total_price + prices[1]
         order_list = order_list + drinks[1] + '\n'
+<<<<<<< HEAD
+>>>>>>> dev01
+=======
 >>>>>>> dev01
     elif menu == "3":
         print("Finish order~")
         break
     else:
 <<<<<<< HEAD
+<<<<<<< HEAD
         print(f"{menu} menu is not exist. please choose a above menu.")
+=======
+        print(f"{menu} menu is not exist. please choose from above menu.")
+
+print(order_list)
+>>>>>>> dev01
 =======
         print(f"{menu} menu is not exist. please choose from above menu.")
 
