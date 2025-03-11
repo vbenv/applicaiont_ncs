6210592255363f472307102c68deca33f007b5c9
vbenv <tjdcksl20@gmail.com>
2025-03-11 23:30:05 +0900

    week02 kiosk v0.6

--- week02.py
+++ week02.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-# 1) Ice Americano : 2000 2) Cafe Latte : 3000
-drinks = ["Ice Americano", "Cafe Latte"]
-prices = [2000, 3000]
-total_price = 0
-while True:
-    menu = input(f"1) {drinks[0]} {prices[0]}  2) {drinks[1]} {prices[1]}  3) Exit : ")
-    if menu == "1":
-        print(f'{drinks[0]} ordered. Price : {prices[0]} won\n')
-        total_price = total_price + prices[0]
-    elif menu == "2":
-        print(f'{drinks[1]} ordered. Price : {prices[1]} won\n')
-        total_price = total_price + prices[1]
-=======
-=======
->>>>>>> dev01
 # 1) Ice Americano : 2000  2) Cafe Latte : 3000
 drinks = ["Ice Americano", "Cafe Latte"]
 prices = [2000, 3000]
@@ -30,25 +13,11 @@ while True:
         print(f"{drinks[1]} ordered. Price : {prices[1]}won")
         total_price = total_price + prices[1]
         order_list = order_list + drinks[1] + '\n'
-<<<<<<< HEAD
->>>>>>> dev01
-=======
->>>>>>> dev01
     elif menu == "3":
         print("Finish order~")
         break
     else:
-<<<<<<< HEAD
-<<<<<<< HEAD
-        print(f"{menu} menu is not exist. please choose a above menu.")
-=======
         print(f"{menu} menu is not exist. please choose from above menu.")
 
 print(order_list)
->>>>>>> dev01
-=======
-        print(f"{menu} menu is not exist. please choose from above menu.")
-
-print(order_list)
->>>>>>> dev01
 print(f"Total price : {total_price}")
