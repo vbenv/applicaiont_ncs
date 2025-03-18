2ed2612492db463d09b550a04f5fe1cea0afe142
vbenv <tjdcksl20@gmail.com>
2025-03-18 18:36:26 +0900

    week03 kiosk v0.9

--- week03.py
+++ week03.py
@@ -3,23 +3,24 @@ drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice"]
 prices = [2000, 3000, 4900]
 amounts = [0, 0, 0]        # Order amounts, 0 : Ice Americano, 1 : Cafe Latte, 2 : Watermelon
 total_price = 0
-# order_list = ''
+
+menu_lists = ''
+for k in range(len(drinks)):
+    menu_lists += f'{k+1}) {drinks[k]} {prices[k]} won '
+    
 while True:
-    menu = input(f"1) {drinks[0]} {prices[0]}won  2) {drinks[1]} {prices[1]}won 3) {drinks[2]} {prices[2]}won 4) Exit : ")
+    menu = input(menu_lists + f"{len(drinks) + 1}) Exit : ")
     if menu == "1":
         print(f"{drinks[0]} ordered. Price : {prices[0]}won")
         total_price = total_price + prices[0]
-        # order_list = order_list + drinks[0] + '\n'
         amounts[0] += 1
     elif menu == "2":
         print(f"{drinks[1]} ordered. Price : {prices[1]}won")
         total_price = total_price + prices[1]
-        # order_list = order_list + drinks[1] + '\n'
         amounts[1] += 1
     elif menu == "3":
         print(f"{drinks[2]} ordered. Price : {prices[2]}won")
         total_price = total_price + prices[2]
-        # order_list = order_list + drinks[1] + '\n'
         amounts[2] += 1
     elif menu == "4":
         print("Finish order~")
@@ -27,12 +28,10 @@ while True:
     else:
         print(f"{menu} menu is not exist. please choose from above menu.")
 
-# print(order_list)
-# print(f"{drinks[0]} {prices[0]} x{amounts[0]} {prices[0] * amounts[0]}")
-# print(f"{drinks[1]} {prices[1]} x{amounts[1]} {prices[1] * amounts[1]}")
-# print(f"{drinks[2]} {prices[2]} x{amounts[2]} {prices[2] * amounts[2]}")
+
 print("Product  Price  Amount  Subtotal")
 for i in range(len(drinks)):
-    print(f"{drinks[i]} {prices[i]} x{amounts[i]} {prices[i] * amounts[i]}")
+    if amounts[i] > 0:
+        print(f"{drinks[i]} {prices[i]} x{amounts[i]} {prices[i] * amounts[i]}")
     
 print(f"Total price : {total_price}")
