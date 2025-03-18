01aa8bd953e004ac2df717fec75cab8f19328db0
vbenv <tjdcksl20@gmail.com>
2025-03-18 20:29:29 +0900

    week-3 ocp v1.2

--- ocp.py
+++ ocp.py
@@ -5,19 +5,19 @@ def time_measure_decorator(f):
         s = time.time()         # start
         r = f(*args)            # function
         e = time.time()         # end
-        print(e-s)
+        print(f'time : {e-s}')
         return r
     return wrapper
 
 
-@time_measure_decorator
+@time_measure_decorator         # first way to use decorator
 def one_to_n_loop(n):           # O(n)
     result = 0
     for i in range(1, n+1):
         result += i
     return result
 
-@time_measure_decorator
+
 def one_to_n_math(n):           # O(1)
     r = n * (n + 1) // 2
     return r
@@ -26,6 +26,5 @@ def one_to_n_math(n):           # O(1)
 number = int(input("정수 입력 : "))
 print(one_to_n_loop(number))
 
-
-number = int(input("정수 입력 : "))
-print(one_to_n_math(number))
+func = time_measure_decorator(one_to_n_math)
+print(func(number))
